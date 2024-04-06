6467cdc0feac946a52ce4e9ef1e4a8b61ef728eb
Danil Tolmachov <danil.tolmachov45@gmail.com>
2024-04-06 12:30:35 +0300

    Init WebApp.

---
+++ WebApp/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddRazorPages()
+				    .AddRazorRuntimeCompilation();
+		}
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+			// Configure the HTTP request pipeline.
+			if (!env.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(e =>
+				e.MapRazorPages());
+		}
+    }
+}
